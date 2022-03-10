926df40439f37469104b6c5fc5343f22c43f43d5
VictorG53 <victor.girault536@gmail.com>
2022-03-10 16:43:42 +0100

    Add pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Index() {
+const Index = () => {
   return (
       <>
           <Head>
@@ -8,11 +9,30 @@ export default function Index() {
               <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           </Head>
           <div className="w-screen h-screen bg-gray-800">
-              <div className="flex flex-col justify-center items-center h-full">
+              <div className="flex flex-col justify-center items-center h-2/3">
                   <h1 className="text-white text-5xl font-medium">Portfolio</h1>
-                  <h2 className="text-white text-4xl font-light">Victor Girault</h2>
+                  <h2 className="text-white text-3xl font-light">Victor Girault</h2>
+              </div>
+              <div className="h-1/3 flex justify-around items-start">
+                  <div className="bg-gray-700 px-5 py-2 rounded drop-shadow">
+                    <Link href="/about">
+                        <a className="text-white text-xl">About Me</a>
+                    </Link>
+                  </div>
+                  <div className="bg-gray-700 px-5 py-2 rounded drop-shadow">
+                    <Link href="/veille">
+                        <a className="text-white text-xl">Veille Technologique</a>
+                    </Link>
+                  </div>
+                  <div className="bg-gray-700 px-5 py-2 rounded drop-shadow">
+                    <Link href="/e4">
+                        <a className="text-white text-xl">E4</a>
+                    </Link>
+                  </div>
               </div>
           </div>
       </>
   )
 }
+
+export default Index;
--- pages/veille.js
+++ pages/veille.js
@@ -1,20 +1,20 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import Link from 'next/link'
 
-const AboutMe = () => {
+const Veille = () => {
     return (
         <>
             <Head>
-                <title>Victor G. - About Me</title>
+                <title>Victor G. - Veille Technologique</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <div className="w-screen h-screen bg-gray-800">
                 <div className="flex justify-center">
-                    <h1 className="mt-4 text-white text-2xl">About Me</h1>
+                    <h1 className="mt-4 text-white text-2xl">Veille Technologique</h1>
                 </div>
             </div>
         </>
     )
 }
 
-export default AboutMe;
+export default Veille;
