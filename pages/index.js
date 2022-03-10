239013686ffeff5f513f02772305d29f0e371a59
VictorG53 <victor.girault536@gmail.com>
2022-03-10 16:02:32 +0100

    Index page done for example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Index() {
   return (
-    <div>
-    </div>
+      <>
+          <Head>
+              <title>Victor G. - Portfolio</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+          <div className="w-screen h-screen bg-gray-800">
+              <div className="flex flex-col justify-center items-center h-full">
+                  <h1 className="text-white text-5xl font-medium">Portfolio</h1>
+                  <h2 className="text-white text-4xl font-light">Victor Girault</h2>
+              </div>
+          </div>
+      </>
   )
 }
