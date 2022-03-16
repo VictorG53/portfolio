5970c0f864fae08fa735e92dd6fde733ecef4d71
VictorG53 <victor.girault536@gmail.com>
2022-03-16 23:01:28 +0100

    Center all texts

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ const Index = () => {
             </Head>
             <div className="w-screen h-screen bg-gradient-to-b from-gray-800 to-gray-900">
                 <div className="flex flex-col justify-center items-center h-5/6 w-full">
-                    <h1 className="text-white text-3xl font-bold">Victor Girault</h1>
-                    <h2 className="text-xl text-white tracking-wider">Student in computer development</h2>
+                    <h1 className="text-white text-center text-3xl font-bold">Victor Girault</h1>
+                    <h2 className="text-xl text-center text-white tracking-wider">Student in computer development</h2>
                     <h3 className="text-md mt-10 text-white text-center font-extralight">Content coming soon</h3>
                 </div>
                 <div className=" flex justify-center items-end h-1/6">
-                    <h3 className="text-white font-extralight">© 2022 Victor Girault</h3>
+                    <h3 className="text-white text-center font-extralight">© 2022 Victor Girault</h3>
                 </div>
             </div>
         </>
