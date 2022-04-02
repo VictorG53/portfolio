4ca0b90921d0ed94bef403e4835fde367d7de77f
VictorG53 <victor.girault536@gmail.com>
2022-04-02 19:23:15 +0200

    Centering text in 404 and redirect pages

--- pages/redirect.js
+++ pages/redirect.js
@@ -3,8 +3,8 @@ import GlowingButton from "../components/glowingButton";
 const Redirect = () => {
     return (
         <div className="h-screen w-screen bg-gray-900 flex flex-col justify-center items-center">
-            <h1 className="text-white font-medium text-5xl">Password required</h1>
-            <p className="text-white font-extralight text-sm">Enter the password before tying to access the portfolio pages</p>
+            <h1 className="text-white font-medium text-5xl text-center">Password required</h1>
+            <p className="text-white font-extralight text-sm text-center">Enter the password before tying to access the portfolio pages</p>
             <GlowingButton link="/" classes="mt-10">Home</GlowingButton>
         </div>
     )
