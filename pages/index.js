1930cb9636fbfbc20a414df988f91fc34c257b74
Victor Girault <victor@rockett.fr>
2022-11-22 22:09:56 +0100

    Delete 'content incomming' text

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,8 @@
-import Head from 'next/head'
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { useLoginContext } from '../components/useContext';
+import Head from 'next/head';
+import React from 'react'
 const Index = () => {
-    const router = useRouter();
-
-    const [password, setPassword] = useState("");
-    const { setValue } = useLoginContext();
-
-    const checkAndRedirect = (e) => {
-        e.preventDefault();
-        if (password) {
-            if (password != process.env.NEXT_PUBLIC_PASSWORD) {
-                alert("Wrong Password");
-            }
-            else {
-                setValue(true);
-                router.push('/portfolio', null, { shallow: true });
-            }
-        }
-    }
-
     return (
-        <>
+        <React.Fragment>
             <Head>
                 <title>Victor G. - Personnal Website</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -30,9 +10,8 @@ const Index = () => {
             <div className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-b from-gray-800 to-gray-900">
                 <h1 className="text-white text-center text-3xl font-bold">Victor Girault</h1>
                 <h2 className="text-xl text-center text-white tracking-wider">Student in developpment</h2>
-                <h5 className="text-lg text-center text-gray-500 tracking-wider mt-10">Upcoming content</h5>
             </div>
-        </>
+        </React.Fragment>
     )
 }
 
